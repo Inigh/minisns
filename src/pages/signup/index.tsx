1ee567ad97565c4b9a2b5c9807f93ddb2e63bf8a
윤효진 <hyojinee@dgsw.hs.kr>
2025-02-08 23:22:12 +0900

    feat: 회원가입 로직 기초

---
+++ src/pages/signup/index.tsx
@@ -0,0 +1,9 @@
+import { useState, useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
+import * as S from './style';
+
+const Signup = () => {
+    const navigate = useNavigate();
+}
+
+export default Signup;
